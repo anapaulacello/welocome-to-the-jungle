c93554193b47cedac4d83e6b0a61b817effc04fc
Ana Paula Morales Dulzaides <anapaulamoralesdulzaidesrgmail.com>
2021-11-27 19:12:55 +0100

    cors

--- server/index.js
+++ server/index.js
@@ -23,10 +23,10 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
 });
-  app.use(cors({
+/*   app.use(cors({
     origin: ['http://localhost:3000','http://localhost:3001'],
     credentials: true,
-})); 
+}));  */
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

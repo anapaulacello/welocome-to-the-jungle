ba7dd78c80b3f05387d68776d231a0b5cc1091d3
Ana Paula Morales Dulzaides <anapaulamoralesdulzaidesrgmail.com>
2021-11-27 19:10:18 +0100

    user sin itinerary

--- server/app/api/models/user.model.js
+++ server/app/api/models/user.model.js
@@ -7,8 +7,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     name: { type: String, trim: true, required: true },
     email: { type: String, trim: true, required: true, unique: true },
-    password: { type: String, trim: true, required: true },
-    itinerary: [{ type: Schema.Types.ObjectId, ref: "day", unique:true}]
+    password: { type: String, trim: true, required: true }
     
 });
 

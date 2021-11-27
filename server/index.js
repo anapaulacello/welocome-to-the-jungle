8dd98f8f526edaf15fa64cd0f8ab10e902d59511
Ana Paula Morales Dulzaides <anapaulamoralesdulzaidesrgmail.com>
2021-11-27 03:38:23 +0100

    acces control allow origin *

--- server/index.js
+++ server/index.js
@@ -19,13 +19,14 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Origin', '*');
     next();
 });
 
-app.use(cors({
-    origin: ['http://localhost:4200','http://localhost:3000','https://conoce-cordoba.vercel.app'],
+/* app.use(cors({
+    origin: ['http://localhost:4200','http://localhost:3000'],
     credentials: true,
-}));
+})); */
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

da01884a7dc52ea6106f00f94c0c786f8e9e001c
Ana Paula Morales Dulzaides <anapaulamoralesdulzaidesrgmail.com>
2021-11-27 15:58:10 +0100

    commit donde sirve vercel

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
 });
-
+ 
 /* app.use(cors({
     origin: ['http://localhost:4200','http://localhost:3000'],
     credentials: true,
